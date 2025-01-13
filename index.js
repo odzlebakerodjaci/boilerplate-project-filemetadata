09a4e15472685594a3ae7f606b9b3a0ed0e23bca
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-13 19:52:13 +0100

    f

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const path = require('path')
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage});
 
 var app = express();
 
@@ -11,7 +16,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  if(!req.file){
+    return res.status(400).send('No file uploaded');
+  }
 
+  const fileMetadata = {
+    name: req.file.originalname,
+    size: req.file.size,
+    type: req.file.mimetype
+  }
+
+  res.json(fileMetadata)
+})
 
 
 const port = process.env.PORT || 3000;
